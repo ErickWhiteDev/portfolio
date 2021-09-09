e321eefdd733d4f634f79300011a3bac58c8496b
Starfire_1234 <58522502+ErickWhiteDev@users.noreply.github.com>
2021-09-09 10:10:02 -0600

    Updated website
    *Updated page titles
    *Updated JavaScript to carry theme across pages

--- website/js/main.js
+++ website/js/main.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) { // https://www.pullrequest.com/blog/how-to-implement-dark-mode-with-css-js/
-    document.documentElement.setAttribute("data-theme", "dark");
 
-    var themeSwitch = document.getElementById("theme-switch");
+    let changeTheme = localStorage.getItem("changeTheme");
+
+    if (changeTheme === null) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    }
+    else {
+      document.documentElement.setAttribute("data-theme", changeTheme);
+    }
+
+    let themeSwitch = document.getElementById("theme-switch");
 
     themeSwitch.onclick = function() {
-      var changeTheme = document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"
+      changeTheme = document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"
       document.documentElement.setAttribute("data-theme", changeTheme);
+      localStorage.setItem("changeTheme", changeTheme);
     }
   });
