08f4e8f6c303541cc40f53554da9181b2e2de1cc
Starfire_1234 <58522502+ErickWhiteDev@users.noreply.github.com>
2021-11-17 09:14:42 -0700

    Finished temperature converter

--- portfolio/challenges/programming_challenges/java/v_1_3/TemperatureConverter03v1.java
+++ portfolio/challenges/programming_challenges/java/v_1_3/TemperatureConverter03v1.java
@@ -4,17 +4,41 @@ import java.util.Scanner;
 
 public class TemperatureConverter03v1 {
     public static void main(String[] args){
+
+        int sel = 3;
+
+        double[] addToCelsius = {-273.15, 0, -32};
+        double[] multToCelsius = {1, 1, .5556};
+
+        double[] multConversion = {1, 1, 1.8};
+        double[] addConversion = {273.15, 0, 32};
+
+        String[] scales = {"Kelvin", "Celsius", "Fahrenheit"};
         
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Input unit: ");
-        String inputUnit = sc.nextLine().toLowerCase();
-        System.out.print("Output unit: ");
-        String outputUnit = sc.nextLine().toLowerCase();
-        int temp = getTemp();
+        while (sel == 3){
+            System.out.print("Input unit: ");
+            sel = getScale(sc.nextLine().toLowerCase());
+        }
+        int inputUnit = sel;
+
+        sel = 3;
+        while (sel == 3){
+            System.out.print("Output unit: ");
+            sel = getScale(sc.nextLine().toLowerCase());
+        }
+        int outputUnit = sel;
+
+        double temp = getTemp();
+        double output = ((temp + addToCelsius[inputUnit]) * multToCelsius[inputUnit]) * multConversion[outputUnit] + addConversion[outputUnit];
+
+        System.out.printf("%.2f degrees in %s is %.2f degrees in %s.", temp, scales[inputUnit], output, scales[outputUnit]);
+
+        sc.close();
     }
 
-    private static int getTemp(){
+    private static double getTemp(){
 
         boolean validInput = false;
         Scanner sc = new Scanner(System.in);
@@ -22,7 +46,7 @@ public class TemperatureConverter03v1 {
         while (!validInput){
             try {
                 System.out.print("Enter a temperature: ");
-                int temp = Integer.parseInt(sc.nextLine());
+                double temp = Double.parseDouble(sc.nextLine());
                 validInput = true;
                 sc.close();
                 return temp;
@@ -32,5 +56,20 @@ public class TemperatureConverter03v1 {
             }
         }
         return 0;
+    }
+
+    private static int getScale(String input){
+        if (input.contains("kelvin") || (input.charAt(0) == 'k')) {
+            return 0;
+        }
+        else if (input.contains("celsius") ||(input.charAt(0) == 'c')) {
+            return 1;
+        }
+        else if (input.contains("fahrenheit") || (input.charAt(0) == 'f')) {
+            return 2;
+        }
+        else {
+            return 3;
+        }
     }
 }
