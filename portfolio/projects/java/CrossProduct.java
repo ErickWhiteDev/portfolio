8f40c8f563b05746eb507b412c405926273c02ec
Erick White <erickhwhite@gmail.com>
2022-01-30 19:07:06 -0700

    Updated CrossProduct.java so it actually works
    *Changed some -s into *s and a + into a - so that the cross-product is actually calculated correctly

--- portfolio/projects/java/CrossProduct.java
+++ portfolio/projects/java/CrossProduct.java
@@ -21,14 +21,14 @@ public class CrossProduct{
     }
 
     private static void printVector(double[] vector){
-        System.out.printf("%.2fi + %.2fj + %.2fk", vector[0], vector[1], vector[2]); // Formats vector
+        System.out.printf("%.2fi - %.2fj + %.2fk", vector[0], vector[1], vector[2]); // Formats vector
     }
 
     private static double[] crossProduct(double[] vectorA, double[] vectorB){
 
-        double i = (vectorA[1] * vectorB[2]) - (vectorA[2] - vectorB[1]); // Calculates i component of cross product
-        double j = (vectorA[2] * vectorB[0]) - (vectorA[0] - vectorB[2]); // Calculates j component of cross product
-        double k = (vectorA[0] * vectorB[1]) - (vectorA[1] - vectorB[0]); // Calculates k component of cross product
+        double i = (vectorA[1] * vectorB[2]) - (vectorA[2] * vectorB[1]); // Calculates i component of cross product
+        double j = (vectorA[2] * vectorB[0]) - (vectorA[0] * vectorB[2]); // Calculates j component of cross product
+        double k = (vectorA[0] * vectorB[1]) - (vectorA[1] * vectorB[0]); // Calculates k component of cross product
 
         return new double[] {i, j, k};
     }
