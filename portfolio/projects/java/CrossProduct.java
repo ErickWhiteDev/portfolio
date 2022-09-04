f32b2ff505a5c1875171bc9f3027fdd9df562e59
Erick White <erickwhitebusiness@gmail.com>
2022-09-03 22:15:02 -0600

    Added AOC 2022 and JS coding challenges

--- portfolio/projects/java/CrossProduct.java
+++ portfolio/projects/java/CrossProduct.java
@@ -9,8 +9,8 @@ public class CrossProduct{
 
     public static void main(String[] args){
 
-        double[] vectorA = {3, 1, 2}; // First vector
-        double[] vectorB = {2, -3, -5}; // Second vector
+        double[] vectorA = {1, 3, 5}; // First vector
+        double[] vectorB = {1, -2, -3}; // Second vector
 
         System.out.print("The cross product of ");
         printVector(vectorA);
@@ -21,15 +21,19 @@ public class CrossProduct{
     }
 
     private static void printVector(double[] vector){
-        System.out.printf("%.2fi - %.2fj + %.2fk", vector[0], vector[1], vector[2]); // Formats vector
+        String a = String.format("%.2fi ", vector[0]);
+        String b = vector[1] > 0 ? String.format("+ %.2fj ", vector[1]) : String.format("- %.2fj ", 0 - vector[1]);
+        String c = vector[2] > 0 ? String.format("+ %.2fk", vector[2]) : String.format("- %.2fk", 0 - vector[2]);
+
+        System.out.print(a + b + c); // Formats vector
     }
 
     private static double[] crossProduct(double[] vectorA, double[] vectorB){
 
         double i = (vectorA[1] * vectorB[2]) - (vectorA[2] * vectorB[1]); // Calculates i component of cross product
-        double j = (vectorA[2] * vectorB[0]) - (vectorA[0] * vectorB[2]); // Calculates j component of cross product
+        double j = (vectorA[0] * vectorB[2]) - (vectorA[2] * vectorB[0]); // Calculates j component of cross product
         double k = (vectorA[0] * vectorB[1]) - (vectorA[1] * vectorB[0]); // Calculates k component of cross product
 
-        return new double[] {i, j, k};
+        return new double[] {i, 0 - j, k};
     }
 }
